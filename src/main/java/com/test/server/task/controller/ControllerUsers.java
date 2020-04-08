0bbc087a66aa42a0a1e1dff3a81b677aaee053ab
yaroslav3 <5647492Opeth>
2020-04-09 01:10:27 +0300

    return page users list, and fix url

--- src/main/java/com/test/server/task/controller/ControllerUsers.java
+++ src/main/java/com/test/server/task/controller/ControllerUsers.java
@@ -6,6 +6,7 @@ import com.test.server.task.service.impl.StatisticServiceImpl;
 import com.test.server.task.service.impl.UsersServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -19,7 +20,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/v1/users/")
+@RequestMapping("api/v1/")
 public class ControllerUsers {
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -32,31 +33,24 @@ public class ControllerUsers {
     this.statisticService = statisticService;
   }
 
-  @GetMapping("/page")
-  public ResponseEntity<List<Users>> getAllUsersPagination(@RequestParam("page") int page, @RequestParam("range") int range) {
+  @GetMapping("/users")
+  public ResponseEntity<Page<Users>> getAllUsersPagination(@RequestParam("page") int page, @RequestParam("range") int range) {
     Pageable pageable = PageRequest.of(page, range);
-    List<Users> users = usersService.getUsers(pageable).stream().map(x -> {
-      return Users.builder()
-        .id(x.getId())
-        .email(x.getEmail())
-        .firstName(x.getFirstName())
-        .lastName(x.getLastName())
-        .IpAddress(x.getIpAddress())
-        .gender(x.getGender())
-        .totalClicks(statisticService.totalClicks(x.getId()))
-        .totalPageViews(statisticService.allTotalPageViews(x.getId())).build();
-    }).collect(Collectors.toList());
-
+    Page<Users> users = usersService.getUsers(pageable).map(x -> {
+      x.setTotalClicks(statisticService.totalClicks(x.getId()));
+      x.setTotalPageViews(statisticService.allTotalPageViews(x.getId()));
+      return x;
+    });
     return new ResponseEntity<>(users, HttpStatus.OK);
   }
 
-  @GetMapping("/statistic")
+  @GetMapping("/users/statistic")
   public ResponseEntity<List<Statistic>> getStatisticUser(@RequestParam("id") long id,
-                                                          @RequestParam("start") String start,
-                                                          @RequestParam("end") String end) {
+                                                          @RequestParam("from") String from,
+                                                          @RequestParam("to") String to) {
     Users users = new Users();
     users.setId(id);
-    List<Statistic> statistic = statisticService.statisticAllUserId(users, start, end);
+    List<Statistic> statistic = statisticService.statisticAllUserId(users, from, to);
     if (!statistic.isEmpty()) {
       return new ResponseEntity<>(statistic, HttpStatus.OK);
     }
